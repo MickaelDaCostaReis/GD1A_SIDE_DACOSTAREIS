2c82b72d0d33d3986f4561a505937c1d65d1d2f3
MickaelDaCostaReis <148327046+MickaelDaCostaReis@users.noreply.github.com>
2024-02-21 12:33:17 +0100

    parallax and tilemap

---
+++ Assets/Parallax.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxCoef;
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+  
+    void Update()
+    {
+        float tmp = (cam.transform.position.x * (1 - parallaxCoef));
+        float distance = (cam.transform.position.x * parallaxCoef);
+        // pas sûr de ça, a tester aled
+        transform.position = new Vector2(startpos + distance, transform.position.x);
+        if (tmp > startpos + length) startpos += length;
+        else if(tmp< startpos-length) startpos -= length;
+    }
+}
